574fc3b6c6aec488c328033ce1bd3be085f04f1d
LucianoWebDev <LucianoWebDev@users.noreply.github.com>
2020-06-19 19:15:48 +0200

    Some small changes

--- master.js
+++ master.js
@@ -5,11 +5,7 @@ let result = document.querySelector('.result')
 let del = document.querySelector('.clear')
 let button = document.getElementsByClassName('button')
 let conti = document.querySelector('body')
-
-//* Just for testing - log the current input
-input.addEventListener('input', function () {
-    console.log(eval(input.value))
-})
+let bckspc = document.querySelector('.backspace')
 
 //! Calc input when enter is clicked
 result.addEventListener('click', function () {
@@ -36,4 +32,56 @@ document.querySelectorAll('.sqr').forEach(function (button) {
 //! Delete the whole Input
 del.addEventListener('click', function () {
     input.value = ''
+})
+
+conti.addEventListener('keydown', function (e) {
+    e.keyCode === 27 ? (input.value = '') : null
+})
+
+//! Delete the last char of Input
+bckspc.addEventListener('click', function () {
+    input.value = input.value.slice(0, -1)
+})
+addEventListener('keydown', function (e) {
+    e.keyCode === 08 ? (input.value = input.value.slice(0, -1)) : null
+})
+
+addEventListener('keydown', function (e) {})
+
+//! Put Numbers directly into Input
+//* Only works, when already something in Input
+addEventListener('keydown', function (e) {
+    e.shiftKey && e.keyCode === 49
+        ? (input.value += '+')
+        : e.shiftKey && e.keyCode === 51
+        ? (input.value += '*')
+        : e.shiftKey && e.keyCode === 55
+        ? (input.value += '/')
+        : e.shiftKey && e.keyCode === 53
+        ? (input.value += '%')
+        : e.keyCode === 189
+        ? (input.value += '-')
+        : e.keyCode === 190
+        ? (input.value += '.')
+        : e.keyCode === 48
+        ? (input.value += 0)
+        : e.keyCode === 49
+        ? (input.value += 1)
+        : e.keyCode === 50
+        ? (input.value += 2)
+        : e.keyCode === 51
+        ? (input.value += 3)
+        : e.keyCode === 52
+        ? (input.value += 4)
+        : e.keyCode === 53
+        ? (input.value += 5)
+        : e.keyCode === 54
+        ? (input.value += 6)
+        : e.keyCode === 55
+        ? (input.value += 7)
+        : e.keyCode === 56
+        ? (input.value += 8)
+        : e.keyCode === 57
+        ? (input.value += 9)
+        : null
 })
